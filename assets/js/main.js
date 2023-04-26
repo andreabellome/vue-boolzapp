a7902368bd907b065dd20fc27aeb64923087bec2
Andrea Bellome <andrea.bellome@gmail.com>
2023-04-26 14:09:28 +0200

    F

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+      return {
+        /* variables Vue 3 */
+        
+      }
+    },
+    mounted(){
+        
+    },
+    methods: {
+        /* functions Vue 3 */
+        
+    }
+}).mount('#app')
